7077bab5997421cc6f3c4b71721c2de366b1b84b
rathinrajendran <rethincrajendran@gmail.com>
2019-08-29 16:31:38 +0530

    edit

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    //menu active
+    $('a.menu').click(function() {
+        $('a.menu').removeClass("active");
+        $(this).addClass("active");
+    });
+
+    //menu shrink
+    $(window).scroll(function() {
+        var wscroll = $(this).scrollTop();
+        if (wscroll > 50) {
+            $("nav.fixed-top").addClass("shrink-nav");
+        } else {
+            $("nav.fixed-top").removeClass("shrink-nav");
+        }
+    });
+
+    $("button #other-services").click(function() {
+        console.log("zdfs");
+        $(".other-block").show();
+    });
+
+});
